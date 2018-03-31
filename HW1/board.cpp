9eac82de06ad5ad9fda87c76b7fd8dcf96470386
Yotam S <yotamsechayk@mail.tau.ac.il>
2018-03-31 15:14:58 +0300

    Update to the board implementation

--- HW1/board.cpp
+++ HW1/board.cpp
@@ -1,6 +1,19 @@
 #include "board.h"
 
 /**
+ * @brief Construct a new Board:: Board object
+ * 
+ * @param n The number of rows in the board
+ * @param m The number of colums in the board
+ */
+Board::Board(int n, int m) : _m(m), _n(n) {
+    _board = new Piece*[n];
+    for (int i=0;i < n; ++i) {
+        _board[i] = new Piece[m];
+    }
+}
+
+/**
  * @brief The function creates and places a new piece on the board. 
  * Based on the rules of each piece (found by its type).
  * 
--- HW1/board.h
+++ HW1/board.h
@@ -12,12 +12,12 @@ class Board {
         int _n;
         int _m;
     public:
-        Board(int n, int m) : _n(n), _m(m) { _board = new Pieces[n][m]; }
+        Board(int n, int m);
         ~Board() { delete[]  _board; }
         Piece* operator[](int index) { return _board[index]; }
-        bool isPositionValid(int x, int, y) { return (x <= _n) && (y <= _m); }
+        bool isPositionValid(int x, int y) { return (x <= _n) && (y <= _m); }
         bool placePiece(int type, int x, int y, bool is_joker=false);
-    /* TODO: maybe add more functions */
-}
+        // TODO: maybe add more functions
+};
 
 #endif
