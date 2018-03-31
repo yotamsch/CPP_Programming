8e5d49cbf76609be476b992be4be7e48609c9fc1
Yotam S <yotamsechayk@mail.tau.ac.il>
2018-03-31 17:16:18 +0300

    Simple Makefile added

--- HW1/game.cpp
+++ HW1/game.cpp
@@ -9,6 +9,19 @@
 #include "pieces.h"
 #include <iostream>
 
+using namespace std;
+
 int main(int argc, char** argv) {
-    cout << "Hello World" << endl;
+    Board b1;
+    Board b2(3, 3);
+    Board b3(2,0);
+    cout << "Board b1=("<< b1.getDimentionX() << "," << b1.getDimentionY() << ")" << endl;
+    cout << "Board b2=("<< b2.getDimentionX() << "," << b2.getDimentionY() << ")" << endl;
+    cout << "b1.isPositionValid(1,1)? " << b1.isPositionValid(1,1) << endl;
+    cout << "b1.isPositionValid(1,0)? " << b1.isPositionValid(1,0) << endl;
+    cout << "b2.isPositionValid(1,1)? " << b2.isPositionValid(1,1) << endl;
+    cout << "b2.isPositionValid(4,1)? " << b2.isPositionValid(4,1) << endl;
+    cout << "b2[1][1]=" << b2[1][1] << endl;
+    cout << "End" << endl;
+    return 0;
 }
