55541bac08af69c2fbdeefb5b24c02f9b254a8dc
Yotam S <yotamsechayk@mail.tau.ac.il>
2018-03-31 14:54:41 +0300

    Initial game implementation, main game file

---
+++ HW1/game.cpp
@@ -0,0 +1,14 @@
+/**
+ * @brief The main game file. Handles reading files, building board and processing steps. Prints the requested information.
+ * 
+ * @file game.cpp
+ * @author Yotam
+ * @date 2018-03-31
+ */
+#include "board.h"
+#include "pieces.h"
+#include <iostream>
+
+int main(int argc, char** argv) {
+    cout << "Hello World" << endl;
+}
