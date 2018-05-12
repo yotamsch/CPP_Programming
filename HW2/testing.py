152f89ab18dd2b7d7694e61e2bd49695c7940477
Yotam S <yotamsechayk@mail.tau.ac.il>
2018-05-12 11:51:27 +0300

    add testing python script

---
+++ HW2/testing.py
@@ -0,0 +1,43 @@
+import subprocess
+
+# The local folder in which the tests are in
+prefix = "./hw1_tests"
+# NOTE: The tests are each in a subfolder named 'test_#'
+#       where # is the test number.
+
+# The mode to check, file-vs-file, file-vs-auto, auto-vs-file
+mode = "file-vs-file"
+
+out_path = "%s/rps.output" % (prefix)
+suffix_p1 = "player1.rps_board"
+suffix_p2 = "player2.rps_board"
+suffix_m1 = "player1.rps_moves"
+suffix_m2 = "player2.rps_moves"
+
+t_res = "%s/results.txt" % (prefix)
+t_exp = "%s/expected.txt" % (prefix)
+f_res = open(t_res, "w")
+f_exp = open(t_exp, "w")
+
+for i in range(0,16):
+    s1 = "%s/test_%d/%s" % (prefix, i, suffix_p1)
+    s2 = "%s/test_%d/%s" % (prefix, i, suffix_p2)
+    s3 = "%s/test_%d/%s" % (prefix, i, suffix_m1)
+    s4 = "%s/test_%d/%s" % (prefix, i, suffix_m2)
+    s_orig = "%s/test_%d/rps.expected" % (prefix, i)
+    action = ["./ex2", mode, out_path, s1, s2, s3, s4]
+    print("\nTest %d:\n\t%s" % (i, action))
+    code = subprocess.call(action)
+    if code == 1:
+        continue
+    f_out = open(out_path,"r")
+    lines_out = f_out.readlines()
+    f_orig = open(s_orig, "r")
+    lines_expected = f_orig.readlines()
+    f_res.write("\ntest_ %d\n-----------\n" % i)
+    f_exp.write("\ntest_ %d\n-----------\n" % i)
+    f_res.writelines(lines_out)
+    f_exp.writelines(lines_expected)
+
+f_res.write("\n- End -")
+f_exp.write("\n- End -")
