c564e50d4b2b9417f1a65693ba1b7f8eacba8950
Yotam S <yotamsechayk@mail.tau.ac.il>
2018-06-23 12:29:03 +0300

    finished implementation HW4 added tests

--- HW4/GameBoard.h
+++ HW4/GameBoard.h
@@ -29,50 +29,87 @@ public:
     private:
         PieceInfo<GAME_PIECE>* _pos;
         int _loc;
-        std::function<bool(const std::pair<int, GAME_PIECE>&)> _filter;
+        static std::function<bool(const std::pair<int, GAME_PIECE>&)> filter;
 
     public:
         iterator(PieceInfo<GAME_PIECE>* pos = nullptr, int loc = 0)
             : _pos(pos)
             , _loc(loc)
-            , _filter([](const std::pair<int, GAME_PIECE>&) { return true; })
         {
+            if (_pos == nullptr || _loc >= ROWS * COLS)
+                return;
+            auto r = filter(*(*_pos));
+            while (!r && _loc < ROWS * COLS) {
+                ++_pos;
+                ++_loc;
+                r = filter(*(*_pos));
+            }
         }
         std::tuple<int, int, GAME_PIECE, int> operator*() { return std::make_tuple(row(_loc), col(_loc), (*_pos)->second, (*_pos)->first); }
         iterator operator++()
         {
+            bool r;
             do {
                 ++_pos;
                 ++_loc;
-            } while (!_filter(*(*_pos)) && _loc < COLS * ROWS);
+                r = filter(*(*_pos));
+            } while (!r && _loc < COLS * ROWS);
+            // if reached the end, reset the filter
             if (_loc >= COLS * ROWS) {
-                _filter = [](const std::pair<int, GAME_PIECE>&) { return true; };
+                filter = [](const std::pair<int, GAME_PIECE>&) { return true; };
             }
             return iterator(_pos, _loc);
         }
         bool operator!=(iterator other) const { return _pos != other._pos; }
-        void setFilter(std::function<bool(const std::pair<int, GAME_PIECE>&)> filter)
+        // set the filter of the iterator
+        static void setFilter(std::function<bool(const std::pair<int, GAME_PIECE>&)> f)
         {
-            _filter = filter;
+            filter = f;
         }
 
     private:
         int row(int loc) { return loc / COLS; }
         int col(int loc) { return loc % COLS; }
     };
-    iterator begin() { return &_board.at(0); }
-    iterator end() { return &_board.at(ROWS * COLS - 1) + 1; }
+    // begin iterator
+    iterator begin() { return iterator(&_board.at(0), 0); }
+    // end iterator
+    iterator end() { return iterator(&_board.at(ROWS * COLS - 1) + 1, ROWS * COLS); }
 
+    /**
+     * @brief Get the Piece Info object at positiong (row,col). If no piece exists in that position, returns nullptr.
+     * 
+     * @param row - A 0-based row position
+     * @param col - A 0-based column position
+     * @return PieceInfo<GAME_PIECE> - A unique_ptr to the Piece Info object (const pair<int, piece_type>)
+     */
     PieceInfo<GAME_PIECE> getPiece(int row, int col)
     {
+        // TODO should i handle edge cases?
+        if (!isPositionLegal(row, col)) {
+            return nullptr;
+        }
         if (_board[row * COLS + col] == nullptr) {
             return nullptr;
         }
         return std::move(std::make_unique<std::pair<int, GAME_PIECE>>(_board[row * COLS + col]->first, _board[row * COLS + col]->second));
     }
 
+    /**
+     * @brief Insert a Piece Info object into the board. If a piece was already in the insertion place, it overwrites it. Returns nullptr if no piece was present, otherwise the Piece Info object of the previus piece.
+     * 
+     * @param row - A 0-based row position
+     * @param col - A 0-based column position
+     * @param piece - The piece itself, assuming same type as the board piece type
+     * @param player - the player which the piece belongs to
+     * @return PieceInfo<GAME_PIECE> - The previus piece in that place or nullptr if no piece existed or the position isn't valid
+     */
     PieceInfo<GAME_PIECE> setPiece(int row, int col, GAME_PIECE piece, int player)
     {
+        // TODO should i handle edge cases?
+        if (!isPositionLegal(row, col)) {
+            return nullptr;
+        }
         auto prev_piece = getPiece(row, col);
         _board[row * COLS + col] = std::make_unique<std::pair<int, GAME_PIECE>>(player, piece);
         return std::move(prev_piece);
@@ -80,21 +117,41 @@ public:
 
     GameBoard<ROWS, COLS, GAME_PIECE, PLAYERS>& allPiecesOfPlayer(int playerNum)
     {
-        setFilter([playerNum](const std::pair<int, GAME_PIECE>& p) { return p.first == playerNum; });
+        iterator::setFilter([playerNum](const std::pair<int, GAME_PIECE>& p) { return p.first == playerNum; });
         return *this;
     }
 
     GameBoard<ROWS, COLS, GAME_PIECE, PLAYERS>& allOccureneceOfPiece(GAME_PIECE piece)
     {
-        setFilter([piece](const std::pair<int, GAME_PIECE>& p) { return p.second == piece; });
+        iterator::setFilter([piece](const std::pair<int, GAME_PIECE>& p) { return p.second == piece; });
         return *this;
     }
 
     GameBoard<ROWS, COLS, GAME_PIECE, PLAYERS>& allOccureneceOfPieceForPlayer(GAME_PIECE piece, int playerNum)
     {
-        setFilter([piece,playerNum](const std::pair<int, GAME_PIECE>& p) { return (p.second == piece && p.first == playerNum); });
+        iterator::setFilter([piece, playerNum](const std::pair<int, GAME_PIECE>& p) { return (p.second == piece && p.first == playerNum); });
         return *this;
     }
+
+private:
+    /**
+     * @brief Checks if the given position is legal and within the board dimensions.
+     * 
+     * @param row - A 0-based row position
+     * @param col - A 0-based column position
+     * @return true - Only if 0 <= row < ROWS, 0 <= col < COLS
+     * @return false - Otherwise
+     */
+    bool isPositionLegal(int row, int col)
+    {
+        if (row < 0 || row >= ROWS || col < 0 || col >= COLS) {
+            return false;
+        }
+        return true;
+    }
 };
+
+template <int ROWS, int COLS, typename GAME_PIECE, int PLAYERS>
+std::function<bool(const std::pair<int, GAME_PIECE>&)> GameBoard<ROWS, COLS, GAME_PIECE, PLAYERS>::iterator::filter = [](const std::pair<int, GAME_PIECE>&) -> bool { return true; };
 
 #endif // !__GAME_BOARD_H_
--- HW4/main.cpp
+++ HW4/main.cpp
@@ -4,31 +4,133 @@
 #include "ex4_header.h"
 #include "unit_test_util.h"
 
-using std::string;
 using std::get;
+using std::string;
 
-static bool test1(){
-	GameBoard<12, 7, string, 4> board;
-	int i = 0;
-	for (int row = 0; row < 12; row++){
-		for(int col =0; col < 7; col++){
-			board.setPiece(row, col, "Piece", i);
-			i = (i+1)%4;
-		}
+static bool test1()
+{
+    GameBoard<12, 7, string, 4> board;
+    int i = 0;
+    for (int row = 0; row < 12; row++) {
+        for (int col = 0; col < 7; col++) {
+            board.setPiece(row, col, "Piece", i);
+            i = (i + 1) % 4;
+        }
+    }
+    int occurence[4] = { 0, 0, 0, 0 };
+    for (auto pieceInfo : board) {
+        occurence[get<3>(pieceInfo)]++;
+    }
+    for (int i = 0; i < 4; i++) {
+        if (occurence[i] != 21) {
+            return false;
+        }
+    }
+    return true;
+}
+
+static bool test2()
+{
+    GameBoard<4, 3, char> board;
+    int i = 0;
+    for (int row = 0; row < 4; row++) {
+        for (int col = 0; col < 3; col++) {
+            ASSERT_TRUE(board.setPiece(row, col, 'A'+i, i) == nullptr);
+            i = (i + 1) % 2;
+        }
+    }
+    int occurence[2] = { 0, 0 };
+	int pos=0;
+    for (auto pieceInfo : board) {
+        occurence[get<3>(pieceInfo)]++;
+		ASSERT_TRUE(get<0>(pieceInfo) == pos / 3);
+		ASSERT_TRUE(get<1>(pieceInfo) == pos % 3);
+		ASSERT_TRUE(get<2>(pieceInfo) == 'A'+get<3>(pieceInfo));
+		++pos;
+    }
+    for (int i = 0; i < 2; i++) {
+        ASSERT_FALSE(occurence[i] != 6);
+    }
+	auto a = board.getPiece(0,0);
+	auto b = board.setPiece(0,0,'C',1);
+	ASSERT_TRUE(a->first == b->first && a->second == b->second);
+	ASSERT_TRUE(board.getPiece(-1,10) == nullptr);
+	ASSERT_TRUE(board.setPiece(-1,10,'C',1) == nullptr);
+    return true;
+}
+
+static bool test3() {
+	GameBoard<4, 3, char> board;
+    int i = 0;
+    for (int row = 0; row < 4; row++) {
+        for (int col = 0; col < 3; col++) {
+            ASSERT_TRUE(board.setPiece(row, col, 'A'+i, i) == nullptr);
+            i = (i + 1) % 2;
+        }
+    }
+	int counter = 0;
+	for (auto pieceInfo : board.allPiecesOfPlayer(0)) {
+		ASSERT_TRUE(get<3>(pieceInfo) == 0);
+		++counter;
 	}
-	int occurence[4]= {0,0,0,0};
-	for(auto pieceInfo : board){
-		occurence[get<3>(pieceInfo)]++;
+	ASSERT_TRUE(counter == 6);
+	counter = 0;
+	for (auto pieceInfo : board.allOccureneceOfPiece('A')) {
+		ASSERT_TRUE(get<2>(pieceInfo) == 'A');
+		++counter;
 	}
-	for(int i = 0;i<4;i++){
-		if (occurence[i] != 21){
-			return false;
-		}
+	ASSERT_TRUE(counter == 6);
+	counter = 0;
+	for (auto pieceInfo : board.allOccureneceOfPiece('B')) {
+		ASSERT_TRUE(get<2>(pieceInfo) == 'B');
+		++counter;
 	}
+	ASSERT_TRUE(counter == 6);
+	counter = 0;
+	for (auto pieceInfo : board.allOccureneceOfPieceForPlayer('B', 0)) {
+		ASSERT_TRUE(get<2>(pieceInfo) != 'C');
+		++counter;
+	}
+	ASSERT_TRUE(counter == 0);
+	counter = 0;
+	for (auto pieceInfo : board.allOccureneceOfPieceForPlayer('A', 1)) {
+		ASSERT_TRUE(get<2>(pieceInfo) != 'C');
+		++counter;
+	}
+	ASSERT_TRUE(counter == 0);
+	counter = 0;
+	for (auto pieceInfo : board.allOccureneceOfPieceForPlayer('A', 0)) {
+		ASSERT_TRUE(get<3>(pieceInfo) == 0);
+		ASSERT_TRUE(get<2>(pieceInfo) == 'A');
+		++counter;
+	}
+	ASSERT_TRUE(counter == 6);
+	counter = 0;
+	for (auto pieceInfo : board.allOccureneceOfPieceForPlayer('B', 1)) {
+		ASSERT_TRUE(get<3>(pieceInfo) == 1);
+		ASSERT_TRUE(get<2>(pieceInfo) == 'B');
+		++counter;
+	}
+	ASSERT_TRUE(counter == 6);
+	int occurence[2] = { 0, 0 };
+	int pos=0;
+    for (auto pieceInfo : board) {
+        occurence[get<3>(pieceInfo)]++;
+		ASSERT_TRUE(get<0>(pieceInfo) == pos / 3);
+		ASSERT_TRUE(get<1>(pieceInfo) == pos % 3);
+		ASSERT_TRUE(get<2>(pieceInfo) == 'A'+get<3>(pieceInfo));
+		++pos;
+    }
+    for (int i = 0; i < 2; i++) {
+        ASSERT_FALSE(occurence[i] != 6);
+    }
 	return true;
 }
 
-int main(){
-	RUN_TEST(test1);
-	return 0;
+int main()
+{
+    RUN_TEST(test1);
+    RUN_TEST(test2);
+    RUN_TEST(test3);
+    return 0;
 }
