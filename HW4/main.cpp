745de93036f91854b4d2c699a7c75fd71b54bd22
Tala A <tala.amoury@gmail.com>
2018-06-27 01:51:09 +0300

    further tested, added a few tests

--- HW4/main.cpp
+++ HW4/main.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <utility>
-
+#include <stdio.h>
+#include <string.h>
 #include "ex4_header.h"
 #include "unit_test_util.h"
 
@@ -152,11 +153,142 @@ static bool test4()
     return true;
 }
 
+
+static bool test7() {
+	GameBoard<6, 4, std::string, 3> board;
+    int i = 0;
+    for (int row = 0; row < 6; row++) {
+        for (int col = 0; col < 4; col++) {	
+			std::string s("shwifty");
+			s = s+std::to_string(i);
+			ASSERT_TRUE(board.setPiece(row, col, s, i) == nullptr);
+      		i = (i + 1) % 3;
+        }
+    }
+		int counter = 0;
+	for (auto pieceInfo : board.allPiecesOfPlayer(0)) {
+		ASSERT_TRUE(get<3>(pieceInfo) == 0);
+		++counter;
+	}
+	
+	ASSERT_TRUE(counter == 8);
+
+	counter = 0;
+	for (auto pieceInfo : board.allPiecesOfPlayer(2)) {
+		ASSERT_TRUE(get<3>(pieceInfo) == 2);
+		++counter;
+	}
+	ASSERT_TRUE(counter == 8);
+
+	counter = 0;
+	for (auto pieceInfo : board.allOccureneceOfPiece("shwifty0")) {
+		ASSERT_TRUE(get<2>(pieceInfo).compare("shwifty0") == 0 );
+		++counter;
+	}
+
+	ASSERT_TRUE(counter == 8);
+	counter = 0;
+	for (auto pieceInfo : board.allOccureneceOfPiece("shwifty1")) {
+		ASSERT_TRUE(get<2>(pieceInfo).compare("shwifty1") == 0 );
+		++counter;
+	}
+
+	ASSERT_TRUE(counter == 8);
+	counter = 0;
+	for (auto pieceInfo : board.allOccureneceOfPieceForPlayer("shwifty1", 2)) {
+		ASSERT_TRUE(get<2>(pieceInfo).compare("world") != 0);
+		ASSERT_TRUE(get<2>(pieceInfo).compare("shwifty1") == 0);
+		ASSERT_TRUE(get<3>(pieceInfo) == 2);
+		++counter;
+	}
+
+	ASSERT_TRUE(counter == 0);
+	
+	counter = 0;
+	for (auto pieceInfo : board.allOccureneceOfPieceForPlayer("A str", 1)) {
+		ASSERT_TRUE(get<2>(pieceInfo).compare("C") != 0);
+		++counter;
+	}
+	ASSERT_TRUE(counter == 0);
+
+	counter = 0;
+	for (auto pieceInfo : board.allOccureneceOfPieceForPlayer("shwifty0", 0)) {
+		ASSERT_TRUE(get<3>(pieceInfo) == 0);
+		ASSERT_TRUE(get<2>(pieceInfo).compare("shwifty0") == 0);
+		++counter;
+	}
+	ASSERT_TRUE(counter == 8);
+	counter = 0;
+	for (auto pieceInfo : board.allOccureneceOfPieceForPlayer("shwifty1", 1)) {
+		ASSERT_TRUE(get<3>(pieceInfo) == 1);
+		ASSERT_TRUE(get<2>(pieceInfo).compare("shwifty1") == 0);
+		++counter;
+	}
+	ASSERT_TRUE(counter == 8);
+	
+	int occurence[3] = { 0, 0, 0 };
+	int pos=0;
+    for (auto pieceInfo : board) {
+        occurence[get<3>(pieceInfo)]++;
+		ASSERT_TRUE(get<0>(pieceInfo) == pos / 4);
+		ASSERT_TRUE(get<1>(pieceInfo) == pos % 4);
+		ASSERT_TRUE(get<2>(pieceInfo).compare("shwifty"+std::to_string(get<3>(pieceInfo))) == 0);
+		++pos;
+    }
+    for (int i = 0; i < 3; i++) {
+        ASSERT_FALSE(occurence[i] != 8);
+    }
+	return true;
+}
+
+static bool test5()
+{
+    GameBoard<3, 6, int, 3> board;
+    if(board.setPiece(0, 1, 40, 0) != nullptr){}
+	if(board.setPiece(0, 2, 40, 0) != nullptr){}
+	if(board.setPiece(1, 2, 40, 0) != nullptr){}
+	if(board.setPiece(1, 3, 41, 0) != nullptr){}
+	if(board.setPiece(2, 4, 42, 0) != nullptr){}
+	
+	//if(board.setPiece(0, 3, 40, 1) != nullptr){}
+	if(board.setPiece(2, 2, 41, 1) != nullptr){}
+	if(board.setPiece(1, 4, 42, 1) != nullptr){}
+	if(board.setPiece(1, 1, 43, 1) != nullptr){}
+	
+	if(board.setPiece(0,0 , 40, 2) != nullptr){}
+	if(board.setPiece(0, 4, 41, 2) != nullptr){} //when col is 5 - problem
+	
+    int occurence[3] = { 0, 0, 0 };
+    for (auto pieceInfo : board) {
+		occurence[get<3>(pieceInfo)]++;
+	}
+    for (int i = 0; i < 3; i++) {
+        if(i==0) ASSERT_FALSE(occurence[i] != 5);
+        if(i==1) ASSERT_FALSE(occurence[i] != 3);
+        if(i==2) ASSERT_FALSE(occurence[i] != 2);
+    }
+	auto a = board.getPiece(0,0);
+	auto b = board.setPiece(0,0,67,1);
+	ASSERT_TRUE(a->first == b->first && a->second == b->second);
+	auto c = board.getPiece(1,2);
+	auto d = board.setPiece(1,2,56,0);
+	ASSERT_TRUE(c->first == d->first && c->second == d->second);
+
+	ASSERT_TRUE(board.getPiece(-1,10) == nullptr);
+	ASSERT_TRUE(board.setPiece(-1,10,90,1) == nullptr);
+	
+    return true;
+}
+
+
+
 int main()
 {
     RUN_TEST(test1);
     RUN_TEST(test2);
     RUN_TEST(test3);
     RUN_TEST(test4);
+    RUN_TEST(test5);
+    RUN_TEST(test7);
     return 0;
 }
